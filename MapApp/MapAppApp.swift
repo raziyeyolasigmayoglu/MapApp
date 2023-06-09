3876ae906748b2996cedee0544b234b122861d79
raziyeyolasigmayoglu <raziyeaka89@gmail.com>
2023-06-09 21:35:34 +0100

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by raziye yolasigmazoglu on 09/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
