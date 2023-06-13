471a379d6d67b1a65300fe50af31c54c616201e8
raziyeyolasigmayoglu <raziyeaka89@gmail.com>
2023-06-13 12:01:24 +0100

    adjusting folders

---
+++ MapApp/Models/Location.swift
@@ -0,0 +1,22 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by raziye yolasigmazoglu on 13/06/2023.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+    
+    
+}
