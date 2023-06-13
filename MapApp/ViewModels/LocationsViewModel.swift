226dfcfca13126385830a72d46c9ef7278d9fc9e
raziyeyolasigmayoglu <raziyeaka89@gmail.com>
2023-06-13 17:51:21 +0100

    added map

--- MapApp/ViewModels/LocationsViewModel.swift
+++ MapApp/ViewModels/LocationsViewModel.swift
@@ -6,14 +6,34 @@
 //
 
 import Foundation
+import MapKit
 
 class LocationViewModel: ObservableObject {
     
     @Published var locations: [Location]
     
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLocation = locations.first!
+        
+        self.updateMapRegion(location: locations.first!)
+    }
+    
+    private func updateMapRegion(location: Location) {
+        mapRegion = MKCoordinateRegion(
+            center: location.coordinates,
+            span: mapSpan)
     }
     
 }
--- MapApp/Views/LocationView.swift
+++ MapApp/Views/LocationView.swift
@@ -6,15 +6,22 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationView: View {
     
     @EnvironmentObject private var vm: LocationViewModel
     
     var body: some View {
-        List {
-            ForEach(vm.locations) {
-                Text($0.name)
+        ZStack {
+            Map(coordinateRegion: $vm.mapRegion)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                header
+                .padding()
+                
+                Spacer()
             }
         }
     }
@@ -24,5 +31,24 @@ struct LocationView_Previews: PreviewProvider {
     static var previews: some View {
         LocationView()
             .environmentObject(LocationViewModel())
+    }
+}
+
+
+extension LocationView {
+    
+    private var header: some View {
+        VStack {
+            Text(vm.mapLocation.name + ", " + vm.mapLocation.cityName)
+                .font(.title2)
+                .fontWeight(.black)
+                .foregroundColor(.primary)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity)
+        }
+        .background(.thickMaterial)
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
+        
     }
 }
